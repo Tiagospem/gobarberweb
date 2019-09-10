7bcf2719e5c219e5dfad32db83fd4db7bca8c4b2
Tiagospem <tiagospem@gmail.com>
2019-09-10 15:44:08 -0300

    added redux saga and reactotron config saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,14 @@
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
-if(process.env.NODE_ENV === 'development') {
-    const tron = Reactotron.configure().connect({
-        host: '198.168.100.34'
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect({
+      host: '198.168.100.34'
     });
-    tron.clear();
-    console.tron = tron;
+  tron.clear();
+  console.tron = tron;
 }
---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = { a: true };
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
